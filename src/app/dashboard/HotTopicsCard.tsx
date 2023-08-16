30b9908e6f72dc7cc83526a59a4f89a102177499
Hamed <alvandyhamed@gmail.com>
2023-08-17 02:31:39 +0330

    Feat(Web):add Dashboard

---
+++ src/app/dashboard/HotTopicsCard.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+
+type Props = {};
+
+const HotTopicsCard = (props: Props) => {
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle className="text-2 font-bold">Hot Topics</CardTitle>
+      </CardHeader>
+      <CardDescription className="pl-6">
+        click on a Topic to start a quiz on it
+      </CardDescription>
+      <CardContent className="pl-2">word cloud</CardContent>
+    </Card>
+  );
+};
+
+export default HotTopicsCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
-import { Button } from "@/components/ui/button";
+import SignInButton from "@/components/SigningButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
 import React from "react";
-import { Prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-type Props = {};
-
-function Home({}: Props) {
-  return <Button>Hello world!</Button>;
+export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user) {
+    //that means the user is logged
+    return redirect("/dashboard");
+  }
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Quizmify</CardTitle>
+          <CardDescription>
+            Quzmify Very very important HA Ha Ha Ha Vala Bekhoda Az in behtar
+            Nadarim !
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sing in with Google" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
-
-export default Home;
